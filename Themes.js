66a99bb1efdc0e95a512826f7795fbf05806a089
Nicholas Ferrara <nferrara100@gmail.com>
2019-04-24 15:00:17 +0100

    Still not working with different theme

---
+++ Themes.js
@@ -0,0 +1,660 @@
+import { systemWeights } from "react-native-typography";
+
+export const draftbit = {
+  disabledOpacity: 0.5,
+  spacing: {
+    gutters: 16,
+    text: 4,
+    small: 8,
+    medium: 12,
+    large: 16
+  },
+  borderRadius: {
+    global: 6,
+    button: 24
+  },
+  typography: {
+    body1: {
+      fontSize: 16,
+      lineHeight: 26,
+      letterSpacing: 0,
+      ...systemWeights.regular
+    },
+    body2: {
+      fontSize: 14,
+      lineHeight: 22,
+      letterSpacing: 0,
+      ...systemWeights.regular
+    },
+    button: {
+      fontSize: 14,
+      lineHeight: 16,
+      letterSpacing: 0,
+      ...systemWeights.bold
+    },
+    caption: {
+      fontSize: 12,
+      lineHeight: 16,
+      letterSpacing: 0,
+      ...systemWeights.regular
+    },
+    headline1: {
+      fontSize: 60,
+      lineHeight: 71,
+      letterSpacing: 0,
+      ...systemWeights.bold
+    },
+    headline2: {
+      fontSize: 48,
+      lineHeight: 58,
+      letterSpacing: 0,
+      ...systemWeights.bold
+    },
+    headline3: {
+      fontSize: 34,
+      lineHeight: 40,
+      letterSpacing: 0,
+      ...systemWeights.bold
+    },
+    headline4: {
+      fontSize: 24,
+      lineHeight: 34,
+      letterSpacing: 0,
+      ...systemWeights.bold
+    },
+    headline5: {
+      fontSize: 20,
+      lineHeight: 26,
+      letterSpacing: 0,
+      ...systemWeights.bold
+    },
+    headline6: {
+      fontSize: 16,
+      lineHeight: 24,
+      letterSpacing: 0,
+      ...systemWeights.bold
+    },
+    overline: {
+      fontSize: 12,
+      lineHeight: 16,
+      letterSpacing: 2,
+      ...systemWeights.regular
+    },
+    subtitle1: {
+      fontSize: 16,
+      lineHeight: 26,
+      letterSpacing: 0,
+      ...systemWeights.regular
+    },
+    subtitle2: {
+      fontSize: 14,
+      lineHeight: 22,
+      letterSpacing: 0,
+      ...systemWeights.regular
+    }
+  },
+  colors: {
+    background: "rgba(251, 252, 253, 1)",
+    divider: "rgba(234, 237, 242, 1)",
+    error: "rgba(255, 69, 100, 1)",
+    light: "rgba(165, 173, 183, 1)",
+    lightInverse: "rgba(255, 255, 255, 0.68)",
+    medium: "rgba(70, 78, 88, 1)",
+    mediumInverse: "rgba(255, 255, 255, 0.87)",
+    primary: "rgba(90, 69, 255, 1)",
+    secondary: "rgba(59, 201, 234, 1)",
+    strong: "rgba(18, 20, 44, 1)",
+    strongInverse: "rgba(255, 255, 255, 1)",
+    surface: "rgba(255, 255, 255, 1)"
+  },
+  elevation: {
+    "0": {
+      shadowColor: "rgba(18, 20, 44, 1)",
+      shadowOffset: {
+        width: 0,
+        height: 0
+      },
+      shadowRadius: 0,
+      shadowOpacity: 0,
+      borderWidth: 0,
+      borderColor: "rgba(18, 20, 44, 1)",
+      borderOpacity: 0
+    },
+    "1": {
+      shadowColor: "rgba(18, 20, 44, 1)",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowRadius: 4,
+      shadowOpacity: 0.06,
+      borderWidth: 1,
+      borderColor: "rgba(18, 20, 44, 1)",
+      borderOpacity: 0.06
+    },
+    "2": {
+      shadowColor: "rgba(18, 20, 44, 1)",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowRadius: 4,
+      shadowOpacity: 0.08,
+      borderWidth: 0,
+      borderColor: "rgba(18, 20, 44, 1)",
+      borderOpacity: 0
+    },
+    "3": {
+      shadowColor: "rgba(18, 20, 44, 1)",
+      shadowOffset: {
+        width: 0,
+        height: 6
+      },
+      shadowRadius: 6,
+      shadowOpacity: 0.12,
+      borderWidth: 0,
+      borderColor: "rgba(18, 20, 44, 1)",
+      borderOpacity: 0
+    }
+  }
+};
+export const draftbit_dark = {
+  disabledOpacity: 0.5,
+  spacing: {
+    gutters: 16,
+    text: 4,
+    small: 8,
+    medium: 12,
+    large: 16
+  },
+  borderRadius: {
+    global: 6,
+    button: 24
+  },
+  typography: {
+    body1: {
+      fontSize: 16,
+      lineHeight: 26,
+      letterSpacing: 0,
+      ...systemWeights.regular
+    },
+    body2: {
+      fontSize: 14,
+      lineHeight: 22,
+      letterSpacing: 0,
+      ...systemWeights.regular
+    },
+    button: {
+      fontSize: 14,
+      lineHeight: 16,
+      letterSpacing: 0,
+      ...systemWeights.bold
+    },
+    caption: {
+      fontSize: 12,
+      lineHeight: 16,
+      letterSpacing: 0,
+      ...systemWeights.regular
+    },
+    headline1: {
+      fontSize: 60,
+      lineHeight: 71,
+      letterSpacing: 0,
+      ...systemWeights.bold
+    },
+    headline2: {
+      fontSize: 48,
+      lineHeight: 58,
+      letterSpacing: 0,
+      ...systemWeights.bold
+    },
+    headline3: {
+      fontSize: 34,
+      lineHeight: 40,
+      letterSpacing: 0,
+      ...systemWeights.bold
+    },
+    headline4: {
+      fontSize: 24,
+      lineHeight: 34,
+      letterSpacing: 0,
+      ...systemWeights.bold
+    },
+    headline5: {
+      fontSize: 20,
+      lineHeight: 26,
+      letterSpacing: 0,
+      ...systemWeights.bold
+    },
+    headline6: {
+      fontSize: 16,
+      lineHeight: 24,
+      letterSpacing: 0,
+      ...systemWeights.bold
+    },
+    overline: {
+      fontSize: 12,
+      lineHeight: 16,
+      letterSpacing: 2,
+      ...systemWeights.regular
+    },
+    subtitle1: {
+      fontSize: 16,
+      lineHeight: 26,
+      letterSpacing: 0,
+      ...systemWeights.regular
+    },
+    subtitle2: {
+      fontSize: 14,
+      lineHeight: 22,
+      letterSpacing: 0,
+      ...systemWeights.regular
+    }
+  },
+  colors: {
+    background: "rgba(20, 21, 33, 1)",
+    divider: "rgba(128, 137, 149, 0.3)",
+    error: "rgba(255, 69, 100, 1)",
+    light: "rgba(83, 96, 112, 1)",
+    lightInverse: "rgba(255, 255, 255, 0.68)",
+    medium: "rgba(154, 163, 177, 1)",
+    mediumInverse: "rgba(255, 255, 255, 0.87)",
+    primary: "rgba(90, 69, 255, 1)",
+    secondary: "rgba(59, 201, 234, 1)",
+    strong: "rgba(255, 255, 255, 1)",
+    strongInverse: "rgba(255, 255, 255, 1)",
+    surface: "rgba(255, 255, 255, 1)"
+  },
+  elevation: {
+    "0": {
+      shadowColor: "rgba(255, 255, 255, 1)",
+      shadowOffset: {
+        width: 0,
+        height: 0
+      },
+      shadowRadius: 0,
+      shadowOpacity: 0,
+      borderWidth: 0,
+      borderColor: "rgba(255, 255, 255, 1)",
+      borderOpacity: 0
+    },
+    "1": {
+      shadowColor: "rgba(255, 255, 255, 1)",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowRadius: 4,
+      shadowOpacity: 0.06,
+      borderWidth: 1,
+      borderColor: "rgba(255, 255, 255, 1)",
+      borderOpacity: 0.06
+    },
+    "2": {
+      shadowColor: "rgba(255, 255, 255, 1)",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowRadius: 4,
+      shadowOpacity: 0.08,
+      borderWidth: 0,
+      borderColor: "rgba(255, 255, 255, 1)",
+      borderOpacity: 0
+    },
+    "3": {
+      shadowColor: "rgba(255, 255, 255, 1)",
+      shadowOffset: {
+        width: 0,
+        height: 6
+      },
+      shadowRadius: 6,
+      shadowOpacity: 0.12,
+      borderWidth: 0,
+      borderColor: "rgba(255, 255, 255, 1)",
+      borderOpacity: 0
+    }
+  }
+};
+export const fire = {
+  disabledOpacity: 0.5,
+  spacing: {
+    gutters: 16,
+    text: 8,
+    small: 16,
+    medium: 16,
+    large: 24
+  },
+  borderRadius: {
+    global: 8,
+    button: 8
+  },
+  typography: {
+    body1: {
+      fontSize: 15,
+      lineHeight: 26,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "PoppinsRegular"
+    },
+    body2: {
+      fontSize: 13,
+      lineHeight: 22,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "PoppinsRegular"
+    },
+    button: {
+      fontSize: 15,
+      lineHeight: 16,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "PoppinsSemiBold"
+    },
+    caption: {
+      fontSize: 13,
+      lineHeight: 16,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "System"
+    },
+    headline1: {
+      fontSize: 60,
+      lineHeight: 71,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "PoppinsSemiBold"
+    },
+    headline2: {
+      fontSize: 48,
+      lineHeight: 58,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "PoppinsSemiBold"
+    },
+    headline3: {
+      fontSize: 40,
+      lineHeight: 46,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "PoppinsSemiBold"
+    },
+    headline4: {
+      fontSize: 24,
+      lineHeight: 46,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "PoppinsSemiBold"
+    },
+    headline5: {
+      fontSize: 20,
+      lineHeight: 26,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "PoppinsSemiBold"
+    },
+    headline6: {
+      fontSize: 14,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "PoppinsSemiBold"
+    },
+    overline: {
+      fontSize: 12,
+      lineHeight: 16,
+      letterSpacing: 2,
+      fontWeight: null,
+      fontFamily: "PoppinsMedium"
+    },
+    subtitle1: {
+      fontSize: 15,
+      lineHeight: 26,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "PoppinsRegular"
+    },
+    subtitle2: {
+      fontSize: 13,
+      lineHeight: 22,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "System"
+    }
+  },
+  colors: {
+    background: "rgba(246, 248, 253, 1)",
+    divider: "rgba(157, 160, 189, 1)",
+    error: "rgba(255, 107, 89, 1)",
+    light: "rgba(155, 160, 195, 1)",
+    lightInverse: "rgba(255, 255, 255, 0.68)",
+    medium: "rgba(102, 107, 149, 1)",
+    mediumInverse: "rgba(255, 255, 255, 0.87)",
+    primary: "rgba(252, 82, 115, 1)",
+    secondary: "rgba(122, 210, 206, 1)",
+    strong: "rgba(27, 33, 75, 1)",
+    strongInverse: "rgba(255, 255, 255, 1)",
+    surface: "rgba(255, 255, 255, 1)"
+  },
+  elevation: {
+    "0": {
+      shadowColor: "rgba(27, 33, 75, 1)",
+      shadowOffset: {
+        width: 0,
+        height: 0
+      },
+      shadowRadius: 0,
+      shadowOpacity: 0,
+      borderWidth: 0,
+      borderColor: "rgba(27, 33, 75, 1)",
+      borderOpacity: 0
+    },
+    "1": {
+      shadowColor: "rgba(27, 33, 75, 1)",
+      shadowOffset: {
+        width: 0,
+        height: 4
+      },
+      shadowRadius: 6,
+      shadowOpacity: 0.06,
+      borderWidth: 1,
+      borderColor: "rgba(27, 33, 75, 1)",
+      borderOpacity: 0.06
+    },
+    "2": {
+      shadowColor: "rgba(27, 33, 75, 1)",
+      shadowOffset: {
+        width: 0,
+        height: 4
+      },
+      shadowRadius: 8,
+      shadowOpacity: 0.08,
+      borderWidth: 0,
+      borderColor: "rgba(27, 33, 75, 1)",
+      borderOpacity: 0
+    },
+    "3": {
+      shadowColor: "rgba(27, 33, 75, 1)",
+      shadowOffset: {
+        width: 0,
+        height: 6
+      },
+      shadowRadius: 8,
+      shadowOpacity: 0.1,
+      borderWidth: 0,
+      borderColor: "rgba(27, 33, 75, 1)",
+      borderOpacity: 0
+    }
+  }
+};
+export const new_theme = {
+  disabledOpacity: 0.5,
+  spacing: {
+    gutters: 16,
+    text: 4,
+    small: 8,
+    medium: 12,
+    large: 16
+  },
+  borderRadius: {
+    global: 6,
+    button: 24
+  },
+  typography: {
+    body1: {
+      fontSize: 16,
+      lineHeight: 26,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "System"
+    },
+    body2: {
+      fontSize: 14,
+      lineHeight: 22,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "System"
+    },
+    button: {
+      fontSize: 14,
+      lineHeight: 16,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "System"
+    },
+    caption: {
+      fontSize: 12,
+      lineHeight: 16,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "System"
+    },
+    headline1: {
+      fontSize: 60,
+      lineHeight: 71,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "System"
+    },
+    headline2: {
+      fontSize: 48,
+      lineHeight: 58,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "System"
+    },
+    headline3: {
+      fontSize: 34,
+      lineHeight: 40,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "System"
+    },
+    headline4: {
+      fontSize: 24,
+      lineHeight: 34,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "System"
+    },
+    headline5: {
+      fontSize: 20,
+      lineHeight: 26,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "System"
+    },
+    headline6: {
+      fontSize: 16,
+      lineHeight: 24,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "System"
+    },
+    overline: {
+      fontSize: 12,
+      lineHeight: 16,
+      letterSpacing: 2,
+      fontWeight: null,
+      fontFamily: "System"
+    },
+    subtitle1: {
+      fontSize: 16,
+      lineHeight: 26,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "System"
+    },
+    subtitle2: {
+      fontSize: 14,
+      lineHeight: 22,
+      letterSpacing: 0,
+      fontWeight: null,
+      fontFamily: "System"
+    }
+  },
+  colors: {
+    background: "rgba(251, 252, 253, 1)",
+    divider: "rgba(234, 237, 242, 1)",
+    error: "rgba(255, 69, 100, 1)",
+    light: "rgba(165, 173, 183, 1)",
+    lightInverse: "rgba(255, 255, 255, 0.68)",
+    medium: "rgba(70, 78, 88, 1)",
+    mediumInverse: "rgba(255, 255, 255, 0.87)",
+    primary: "rgba(90, 69, 255, 1)",
+    secondary: "rgba(59, 201, 234, 1)",
+    strong: "rgba(18, 20, 44, 1)",
+    strongInverse: "rgba(255, 255, 255, 1)",
+    surface: "rgba(255, 255, 255, 1)"
+  },
+  elevation: {
+    "0": {
+      shadowColor: "rgba(18, 20, 44, 1)",
+      shadowOffset: {
+        width: 0,
+        height: 0
+      },
+      shadowRadius: 0,
+      shadowOpacity: 0,
+      borderWidth: 0,
+      borderColor: "rgba(18, 20, 44, 1)",
+      borderOpacity: 0
+    },
+    "1": {
+      shadowColor: "rgba(18, 20, 44, 1)",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowRadius: 4,
+      shadowOpacity: 0.06,
+      borderWidth: 1,
+      borderColor: "rgba(18, 20, 44, 1)",
+      borderOpacity: 0.06
+    },
+    "2": {
+      shadowColor: "rgba(18, 20, 44, 1)",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowRadius: 4,
+      shadowOpacity: 0.08,
+      borderWidth: 0,
+      borderColor: "rgba(18, 20, 44, 1)",
+      borderOpacity: 0
+    },
+    "3": {
+      shadowColor: "rgba(18, 20, 44, 1)",
+      shadowOffset: {
+        width: 0,
+        height: 6
+      },
+      shadowRadius: 6,
+      shadowOpacity: 0.12,
+      borderWidth: 0,
+      borderColor: "rgba(18, 20, 44, 1)",
+      borderOpacity: 0
+    }
+  }
+};
