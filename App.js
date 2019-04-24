df4ab96958c2980927002f26a0e14bd358a19e8d
Nicholas Ferrara <nferrara100@gmail.com>
2019-04-24 14:47:56 +0100

    Failing Draftbit

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "@draftbit/ui";
+import { draftbit_dark as AppTheme } from "./Themes";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider theme={AppTheme}>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+      </Provider>
     );
   }
 }
@@ -14,8 +18,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
